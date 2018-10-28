d2f1211efea9c4de0218fc2e58ca1c7a568cd213
ShambaC <38806897+ShambaC@users.noreply.github.com>
2018-10-28 18:28:55 +0530

    First comit
    Comit for You Tube

---
+++ tutpro/Assets/Scripts/Drawnum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Drawnum : MonoBehaviour {
+
+    public Text CurrentScore;
+    public Text HighScore;
+
+	// Use this for initialization
+	void Start ()
+    {
+        int a1 = PlayerPrefs.GetInt("Highscore", 0);
+        HighScore.text = a1.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //Button function
+    public void Draw()
+    {
+        int a = Random.Range(1, 1000);
+        CurrentScore.text = a.ToString();
+        int a1 = PlayerPrefs.GetInt("Highscore", 0);
+        if(a > a1)
+        {
+            HighScore.text = a.ToString();
+            PlayerPrefs.SetInt("Highscore", a);
+        }
+    }
+}
